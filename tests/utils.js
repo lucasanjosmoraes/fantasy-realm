8926e5056892c117258f6b788341a0585b75385d
Lucas dos Anjos Moraes <lucas.moraes.ext@neon.com.br>
2020-01-29 17:38:28 -0300

    Create functions to help to develop tests

---
+++ tests/utils.js
@@ -0,0 +1,13 @@
+const identity = (x) => x;
+const double = (el) => 2 * el;
+const triple = (el) => 3 * el;
+const half = (x) => x / 2;
+const tenTimes = (x) => 10 * x;
+
+module.exports = {
+  identity,
+  double,
+  triple,
+  half,
+  tenTimes,
+};
