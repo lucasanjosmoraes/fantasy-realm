ac0e5daefa83bb619e6e8c09a174d0dea3d8cf75
Lucas dos Anjos Moraes <lucas.moraes.ext@neon.com.br>
2020-02-04 16:17:26 -0300

    Split functions as modules and write package

--- usage/cli.js
+++ usage/cli.js
@@ -1,5 +1,6 @@
-/* eslint no-unused-vars:0 */
+/* eslint no-unused-vars:0 no-process-exit:0 */
 const Task = require('data.task');
+const { writeFile } = require('fs');
 
 const showInternalError = () => {
   console.log('Sorry, we couldn\'t create the file');
@@ -13,9 +14,8 @@ const rl = require('readline').createInterface({
 const buildPackageJSON = ({
   packageName, version, description, entryPoint, testCommand,
   repository, keywords, author, license,
-}) => (`
-{
-  "name": ${packageName},
+}) => (`{
+  "name": "${packageName}",
   "version": "${version || '1.0.0'}",
   "description": "${description}",
   "main": "${entryPoint || 'index.js'}",
@@ -26,54 +26,93 @@ const buildPackageJSON = ({
   "keywords": "${keywords}",
   "author": "${author}",
   "license": "${license || 'ISC'}"
-}
+}`);
 
-Is this OK? (yes)
-`);
-
-const prompt = new Task((rej, res) => rl.question('>', res));
+const prompt = (previousData) => new Task((rej, res) => (
+  rl.question('> ', (text) => res({ text, previousData }))
+));
 
 const speak = (string) => new Task((rej, res) => res(console.log(string)));
 
-const npmInitApp = speak('package name:').chain((_) => prompt)
-  .chain((packageName) => {
-    return speak('version: (1.0.0)').chain((_) => prompt)
-      .chain((version) => {
-        return speak('description:').chain((_) => prompt)
-          .chain((description) => {
-            return speak('entry point: (index.js)').chain((_) => prompt)
-              .chain((entryPoint) => {
-                return speak('test command:').chain((_) => prompt)
-                  .chain((testCommand) => {
-                    return speak('git repository:').chain((_) => prompt)
-                      .chain((repository) => {
-                        return speak('keywords:').chain((_) => prompt)
-                          .chain((keywords) => {
-                            return speak('author:').chain((_) => prompt)
-                              .chain((author) => {
-                                return speak('license: (ISC)').chain((_) => prompt)
-                                  .chain((license) => {
-                                    return speak(buildPackageJSON(({
-                                      packageName,
-                                      version,
-                                      description,
-                                      entryPoint,
-                                      testCommand,
-                                      repository,
-                                      keywords,
-                                      author,
-                                      license,
-                                    }))).chain((_) => prompt);
-                                  });
-                              });
-                          });
-                      });
-                  });
-              });
-          });
-      });
-  });
+const generatePackage = (packageAsString) => (
+  speak(`${packageAsString}
+    
+Is this OK? (yes)
+  `).chain((_) => prompt(packageAsString))
+);
 
-npmInitApp.fork(showInternalError, () => {
+const mountPackage = ({ text: license, previousData }) => (
+  generatePackage(buildPackageJSON({ license, ...previousData }))
+);
+
+const getLicense = ({ text: author, previousData }) => (
+  speak('license: (ISC)').chain((_) => prompt({ author, ...previousData }))
+    .chain(mountPackage)
+);
+
+const getAuthor = ({ text: keywords, previousData }) => (
+  speak('author:').chain((_) => prompt({ keywords, ...previousData }))
+    .chain(getLicense)
+);
+
+const getKeywords = ({ text: repository, previousData }) => (
+  speak('keywords:').chain((_) => prompt({ repository, ...previousData }))
+    .chain(getAuthor)
+);
+
+const getRepository = ({ text: testCommand, previousData }) => (
+  speak('git repository:').chain((_) => prompt({ testCommand, ...previousData }))
+    .chain(getKeywords)
+);
+
+const getTestCommand = ({ text: entryPoint, previousData }) => (
+  speak('test command:').chain((_) => prompt({ entryPoint, ...previousData }))
+    .chain(getRepository)
+);
+
+const getEntryPoint = ({ text: description, previousData }) => (
+  speak('entry point: (index.js)').chain((_) => prompt({ description, ...previousData }))
+    .chain(getTestCommand)
+);
+
+const getDescription = ({ text: version, previousData }) => (
+  speak('description:').chain((_) => prompt({ version, ...previousData }))
+    .chain(getEntryPoint)
+);
+
+const getVersion = ({ text: packageName, previousData }) => (
+  speak('version: (1.0.0)').chain((_) => prompt({ packageName, ...previousData }))
+    .chain(getDescription)
+);
+
+const getPackageName = (
+  speak('package name:').chain((_) => prompt({}))
+    .chain(getVersion)
+);
+
+const npmInitApp = getPackageName;
+
+const writePackage = (packageJSON) => new Task((rej, res) => (
+  writeFile('./my-package.json', packageJSON, (error) => {
+    if (error) return rej(error);
+
+    return res();
+  })
+));
+
+npmInitApp.fork(showInternalError, ({
+  text: createResponse, previousData: packageAsString,
+}) => {
+  if (
+    [ 'yes', 'y' ].includes(createResponse.toLowerCase())
+    || createResponse === ''
+  ) {
+    writePackage(packageAsString).fork(showInternalError, () => {
+      console.log('File created, enjoy :p');
+
+      process.exit(0);
+    });
+  }
+
   rl.close();
 });
