ee6699240ccae9e66b31a571faab0ba3e30aaeb0
Lucas dos Anjos Moraes <lucas.moraes.ext@neon.com.br>
2020-02-05 14:50:21 -0300

    Disable eslint rule on entire profunctor usage file

--- usage/profunctor.js
+++ usage/profunctor.js
@@ -1,17 +1,12 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint no-unused-vars:0 */
 const { Array } = require('../implementations/functor');
 const { Costar } = require('../implementations/profunctor');
 
 const data = [ 1, 3, 5, 7, 8, 1 ];
-
 const sum = new Costar((foldable) => foldable.reduce((x, y) => x + y, 0));
 
-// eslint-disable-next-line no-unused-vars
 const length = sum['fantasy-realm/promap']((_) => 1, (x) => x);
-
 const isGt5 = sum['fantasy-realm/promap']((x) => x, (x) => x > 5);
-
-// eslint-disable-next-line no-unused-vars
 const longEnough = sum['fantasy-realm/promap']((_) => 1, (x) => x > 5);
 
 console.log('length', length.run(data));
